b7e420381a2a4b70d2f574abf78fd41b6c2cdd34
Sayyid Maulana Khakul Yakin <sayyid.yakin@students.amikom.ac.id>
2019-12-29 21:39:53 +0700

    first commit

---
+++ Data Pegawai/AddFormEmployeesController.swift
@@ -0,0 +1,30 @@
+//
+//  AddFormEmployeesController.swift
+//  Data Pegawai
+//
+//  Created by sayyid maulana khakul yakin on 29/12/19.
+//  Copyright © 2019 sayyid maulana khakul yakin. All rights reserved.
+//
+
+import UIKit
+
+class AddFormEmployeesController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
